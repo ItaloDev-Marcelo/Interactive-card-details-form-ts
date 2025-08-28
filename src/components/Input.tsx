63a147a9dd18c1848c7b715b32058086bf647928
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-08-28 14:51:40 -0300

    Agilizando com daisyui estilizando formulario

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,10 +4,11 @@ const Input = ({Data, register,inputErrorSms,style}:DataFormate) => {
   
   return (
     <div className={`flex flex-col ${style}`}>
-        <label htmlFor={Data.id} className="mb-2">{Data.label}</label>
-        <input className="border-1 h-[40px] rounded-[7px]" type={Data.inputType} {...register(Data.name)} id={Data.id} placeholder={Data.placeholder} />
-        <span className="text-red-500">{inputErrorSms[Data.name]?.message}</span>
+        <legend className="mb-1 fieldset-legend">{Data.label}</legend>
+        <input className={`border-1 h-[40px] rounded-[7px] font-semibold input ${inputErrorSms[Data.name] && 'border-red-500' }   ${style}`} type={Data.inputType} {...register(Data.name)} placeholder={Data.placeholder} />
+        <span className="text-red-500  label text-[0.7em] mt-0.5">{inputErrorSms[Data.name]?.message}</span>
     </div>
+
   )
 }
 
--- src/data/inputFormate.ts
+++ src/data/inputFormate.ts
@@ -1,8 +1,8 @@
 import type { FormFormate} from "../interface/inputFormate";
 
 export const inputFormate:FormFormate[] = [
-    {id: '2', name: 'Name', label: 'CARDHOLDER NAME', placeholder: 'e.g ane Applesed', inputType: 'text'},
-    {id: '1', name: 'CardNumber', label: 'CARD NUMBER', placeholder: 'e.g 1234 5678 9123 0000', inputType: 'text'},
+    {id: '2', name: 'Name', label: 'CARDHOLDER NAME', placeholder: 'e.g. Jane Applesed', inputType: 'text'},
+    {id: '1', name: 'CardNumber', label: 'CARD NUMBER', placeholder: 'e.g. 1234 5678 9123 0000', inputType: 'text'},
     {id: '3', name: 'ExperationDateDay', placeholder: 'MM', inputType: 'text'},
     {id: '4', name: 'ExperationDateYear', placeholder: 'YY', inputType: 'text'},
     {id: '5', name: 'Cvc' , label:'CVC', placeholder: 'e.g. 123', inputType: 'text'}
